cd66f65c8abd51573e8be63089f790ca4ab75d91
lucas-cardoso-cst <lucascardosocst@gmail.com>
2025-06-05 13:38:08 -0300

    Delete quantidade.py

--- quantidade.py
+++
@@ -1,6 +0,0 @@
-from operacoesbd import *
-def quantidademanifestações(conexão):
-    consulta = 'select count(*) from manifestacoes'
-    quantidademanifestacoes = listarBancoDados(conexão, consulta)
-    print('Existem', quantidademanifestacoes[0][0], 'manifestações!')
-    
