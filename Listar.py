de6465e63d23c7c0042517b2e197e9533a84892d
João Pedro Almeida Galdino Barbosa <galegojoaopedro41@gmail.com>
2025-06-05 00:20:07 -0300

    Operação para listagem de todas as manifestações

--- Listar.py
+++ Listar.py
@@ -1,6 +1,5 @@
 from operacoesbd import *
 
-conexão = criarConexao('127.0.0.1','root','12345', 'manifestacoes')
 consulta = 'select * from Manifestacoes'
 manifestacoes = listarBancoDados(conexão,consulta)
 if len(manifestacoes) == 0:
@@ -9,5 +8,3 @@ else:
      print("Lista de Manifestações:")
      for item in manifestacoes:
           print("- Código da Manifestação:",item[0],"Titulo da Manifestação:",item[1],"Autor:",item[3],"Tipo da manifestação: ",item[4])
-
-encerrarConexao(conexão)
