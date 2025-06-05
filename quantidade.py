01ce3f16ce03f5aed7c1d93ba02df126256af852
lucas cardoso cst <lucascardosocst@gmail.com>
2025-06-05 13:25:37 -0300

    operação que mostra a quantidade de manifestações

--- quantidade.py
+++ quantidade.py
@@ -3,3 +3,4 @@ def quantidademanifestações(conexão):
     consulta = 'select count(*) from manifestacoes'
     quantidademanifestacoes = listarBancoDados(conexão, consulta)
     print('Existem', quantidademanifestacoes[0][0], 'manifestações!')
+    
