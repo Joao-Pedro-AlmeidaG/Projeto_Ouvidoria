78035c82b0dd89148fc6af9ad935ac6c508f6cc5
joao nascimento <joaopedroabrantes@gmail.com>
2025-06-10 09:15:33 -0300

    Adiciando metodo de listar por tipo

--- Metodos.py
+++ Metodos.py
@@ -13,6 +13,26 @@ def ListarManifestacoes(conexão):
             print("\n- Código da Manifestação:", item[0], "Titulo da Manifestação:", item[1], "Autor:", item[3],",Descrição:", item[2], ",Tipo da manifestação: ", item[4])
 
 
+def listarporTipo(conexão):
+    TipodeManifestacao = int(input("DIgite o tipo da manifestação para listar: 1-Para Elogio, 2- Para Reclamanção, 3- Para Sugestão: "))
+    if TipodeManifestacao == 1:
+        TipodeManifestacao = "Elogio"
+    elif TipodeManifestacao == 2:
+        TipodeManifestacao = "Reclamação"
+    elif TipodeManifestacao == 3:
+        TipodeManifestacao = "Sugestão"
+    consulta = 'select * from manifestacoes where tipo like %s'
+    dados = [TipodeManifestacao]
+    manifestacoes = listarBancoDados(conexão, consulta, dados)
+    if len(manifestacoes) == 0:
+        print("Não existe manifestações para o tipo informado.")
+    else:
+        print("Manifestações encontradas:")
+        for manifestacao in manifestacoes:
+            print("- Título:",manifestacao[1], ",Descrição:",manifestacao[2], ",Autor:",manifestacao[3],",tipo",manifestacao[4])
+
+
+
 def PesquisarManifestacao(conexão):
     codigo = input("Digite o código da manifestação ser pesquisado: ")
     consulta = 'select * from manifestacoes where codigo_manifestacao = %s'
