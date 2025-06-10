7b5ff2ea53ad57d4714eb1b11a96957039f78b70
João Pedro Almeida <146199932+Joao-Pedro-AlmeidaG@users.noreply.github.com>
2025-06-09 22:57:14 -0300

    Update menu.py
    remover lista Inútil

--- menu.py
+++ menu.py
@@ -3,7 +3,6 @@ from Metodos import  *
 conexão = criarConexao('127.0.0.1','root','12345','manifestacoes')
 
 opcao = 1
-filmes = [ ]
 while opcao !=7:
     print("\n1) Listagem das Manifestações \n2) Listagem de Manifestações por Tipo \n3) Criar uma nova Manifestação \n4) Exibir quantidade de manifestações \n5) Pesquisar uma manifestação por código \n6) Excluir uma Manifestação pelo Código \n7) Sair do Sistema ")
     opcao = int(input("Digite sua opção: "))
